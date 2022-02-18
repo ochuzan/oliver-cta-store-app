fb3c3bbf71fd0e56ca94a803cc6b6aa31e44807e
Oliver Chuzan <ochuzan@gmail.com>
2022-02-17 19:53:03 -0500

    Completed schema and seed sql files

--- back-end/db/prod_seed.sql
+++ back-end/db/prod_seed.sql
@@ -1,8 +1,7 @@
-INSERT INTO test (name) VALUES
-('Monday'),
-('Tuesday'),
-('Wednesday'),
-('Thursday'),
-('Friday'),
-('Saturday'),
-('Sunday');
+INSERT INTO test (name, description, price, rating, featured) VALUES
+('Keychron K2', 'Wireless/Wired 75% Layout', 100, 4, false),
+('Logitech K380', 'Wireless Minimalist Layout', 40, 3, false),
+('Wuque Mammoth 75', 'Wireless/Wired 75% Layout', 400, 5, true),
+('Apple Magic Keyboard with Touch ID', 'Wireless Minimalist Layout', 150, 4, false),
+('Glorious GMMK Pro', 'Wired 75% Layout', 170, 4, false),
+
--- back-end/db/schema.sql
+++ back-end/db/schema.sql
@@ -1,11 +1,20 @@
-DROP DATABASE IF EXISTS cta_dev;
-CREATE DATABASE cta_dev;
+DROP DATABASE IF EXISTS keyboards_dev;
+CREATE DATABASE keyboards_dev;
 
-\c cta_dev;
+\c keyboards_dev;
 
 DROP TABLE IF EXISTS test;
 
 CREATE TABLE test (
     id SERIAL PRIMARY KEY, 
-    name TEXT
+    name TEXT,
+    description TEXT,
+    price NUMERIC,
+    rating NUMERIC,
+    CHECK (rating >=0 AND rating <=5),
+    featured BOOLEAN
+    -- color TEXT,
+    -- layout TEXT,
+    -- connectivity TEXT,
+    -- winkeyless BOOLEAN,
 );
