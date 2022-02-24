ac697b6bd6b023c298dcee3b16da76553a9e6329
Oliver Chuzan <ochuzan@gmail.com>
2022-02-23 19:59:06 -0500

    Fixed Carousel styling

--- front-end/src/components/Home.js
+++ front-end/src/components/Home.js
@@ -2,6 +2,7 @@ import { useState, useEffect } from 'react';
 import axios from 'axios';
 import { Carousel } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Link } from 'react-router-dom';
 
 function Home() {
     const [index, setIndex] = useState(0);
@@ -29,61 +30,32 @@ function Home() {
   
     let featuredCarousel = keyboards.map((keyboard) => {
         return (
-            <Carousel.Item key={keyboard.id}>
-                <img
-                    className="d-block w-100 carousel-img"
-                    src={keyboard.image}
-                    alt={keyboard.name}
-                />
-                <Carousel.Caption className="carousel-cap">
-                    <h3>{keyboard.name}</h3>
-                    <p>{keyboard.description}</p>
-                </Carousel.Caption>
-            </Carousel.Item>
+          <Carousel.Item key={keyboard.id}>
+            <Link to={`/keyboards/${keyboard.id}`}>
+                  <img
+                      // className="d-block w-100 carousel-img"
+                      className='carousel-img'
+                      src={keyboard.image}
+                      alt={keyboard.name}
+                  />
+                  <Carousel.Caption className="carousel-cap">
+                      <h3 className="carousel-cap">{keyboard.name}</h3>
+                      <p className="carousel-cap">{keyboard.description}</p>
+                  </Carousel.Caption>
+            </Link>
+          </Carousel.Item>
         )
     })
 
     return (
-      <Carousel variant="dark" activeIndex={index} onSelect={handleSelect}>
-          {featuredCarousel}
-        {/* <Carousel.Item>
-          <img
-            className="d-block w-100"
-            src="holder.js/800x400?text=First slide&bg=373940"
-            alt="First slide"
-          />
-          <Carousel.Caption>
-            <h3>First slide label</h3>
-            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-          </Carousel.Caption>
-        </Carousel.Item>
-        <Carousel.Item>
-          <img
-            className="d-block w-100"
-            src="holder.js/800x400?text=Second slide&bg=282c34"
-            alt="Second slide"
-          />
-  
-          <Carousel.Caption>
-            <h3>Second slide label</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-          </Carousel.Caption>
-        </Carousel.Item>
-        <Carousel.Item>
-          <img
-            className="d-block w-100"
-            src="holder.js/800x400?text=Third slide&bg=20232a"
-            alt="Third slide"
-          />
-  
-          <Carousel.Caption>
-            <h3>Third slide label</h3>
-            <p>
-              Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-            </p>
-          </Carousel.Caption>
-        </Carousel.Item> */}
-      </Carousel>
+      <div>
+        <div className="typewriter">
+          <h2>SlappinKeys</h2>
+        </div>
+        <Carousel variant="dark" activeIndex={index} onSelect={handleSelect}>
+            {featuredCarousel}
+        </Carousel>
+      </div>
     );
 }
 
